c07fd17081fc61225a71414b8b3df3d4822d054d
Andrew Croce <andrew.croce@gmail.com>
2015-05-14 15:17:20 -0400

    get_paged_vars() function

--- includes/helpers.php
+++ includes/helpers.php
@@ -1,5 +1,26 @@
 <?php
 
+
+
+function get_paged_vars( $paged ) {
+
+    $vars = array();
+
+    if( $paged == 0 ) {
+        $vars['page_number'] = 1;
+        $vars['start_number'] = 1;
+    } else {
+        $posts_per_page = get_option('posts_per_page');
+        $vars['page_number'] = $paged;
+        $vars['start_number'] =  (( $posts_per_page * $paged ) - $posts_per_page ) + 1;
+    }
+
+    return $vars;
+}
+
+
+
+
 /**
 *
 * Get a truncated snippet/excerpt from any content
