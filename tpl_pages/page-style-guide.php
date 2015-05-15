9471cfb6ef764ad4afcecfedf166450f48f67916
Andrew Croce <andrew.croce@gmail.com>
2015-05-15 13:00:17 -0400

    Assorted default styles for the style-guide.
    CSS mods for pagination styling

--- tpl_pages/page-style-guide.php
+++ tpl_pages/page-style-guide.php
@@ -11,6 +11,19 @@
 		</div>
 	</div>
 
+	<section id="important-text" class="guide-section">
+		<div class="row">
+			<div class="large-4 large-push-8 columns">
+				<h2 class="guide-section-label">Important Text</h2>
+				<span class="guide-section-sub-label">&lt;p class="important-text"&gt;</span>
+			</div>
+			
+			<div class="large-8 large-pull-4 columns">
+				<p class="important-text">Compellingly target scalable benefits vis-a-vis visionary systems. Credibly supply real-time niche markets after extensible technologies. Professionally integrate backward-compatible convergence vis-a-vis sustainable leadership skills.</p>
+			</div>
+		</div>
+	</section>
+
 
 	<section id="paragraph" class="guide-section">
 		<div class="row">
@@ -25,12 +38,49 @@
 		</div>
 	</section>
 
+	<section id="secondary-text" class="guide-section">
+		<div class="row">
+			<div class="large-4 large-push-8 columns">
+				<h2 class="guide-section-label">Secondary Text</h2>
+				<span class="guide-section-sub-label">&lt;p class="secondary-text"&gt;</span>
+			</div>
+			
+			<div class="large-8 large-pull-4 columns">
+				<p class="secondary-text">Compellingly target scalable benefits vis-a-vis visionary systems. Credibly supply real-time niche markets after extensible technologies. Professionally integrate backward-compatible convergence vis-a-vis sustainable leadership skills. Synergistically visualize an expanded array of products whereas reliable information. Dynamically redefine top-line supply chains after 2.0 deliverables. Dramatically leverage other's leading-edge content vis-a-vis 24/365 models.</p>
+			</div>
+		</div>
+	</section>
+
+	<section id="lists" class="guide-section">
+		<div class="row">
+			<div class="large-4 large-push-8 columns">
+				<h2 class="guide-section-label">Lists</h2>
+				<span class="guide-section-sub-label">&lt;ul&gt;<br>&lt;ol&gt;</span>
+			</div>
+			
+			<div class="large-4 large-pull-4 columns">
+				<ul>
+					<li>Item One</li>
+					<li>Item Two</li>
+					<li>Item Three</li>
+				</ul>
+			</div>
+			<div class="large-4 large-pull-4 columns">
+				<ol>
+					<li>Item One</li>
+					<li>Item Two</li>
+					<li>Item Three</li>
+				</ol>
+			</div>
+		</div>
+	</section>
+
 
 	<section id="large-heading" class="guide-section">
 		<div class="row">
 			<div class="large-4 large-push-8 columns">
 				<h2 class="guide-section-label">Large Heading</h2>
-				<span class="guide-section-sub-label">&lt;h1 class="large-heading"&gt;</span>
+				<span class="guide-section-sub-label">&lt;h{n} class="large-heading"&gt;</span>
 			</div>
 			
 			<div class="large-8 large-pull-4 columns">
@@ -44,7 +94,7 @@
 		<div class="row">
 			<div class="large-4 large-push-8 columns">
 				<h2 class="guide-section-label">Medium Heading</h2>
-				<span class="guide-section-sub-label">&lt;h2 class="medium-heading"&gt;</span>
+				<span class="guide-section-sub-label">&lt;h{n} class="medium-heading"&gt;</span>
 			</div>
 			
 			<div class="large-8 large-pull-4 columns">
@@ -58,7 +108,7 @@
 		<div class="row">
 			<div class="large-4 large-push-8 columns">
 				<h2 class="guide-section-label">Small Heading</h2>
-				<span class="guide-section-sub-label">&lt;h3 class="small-heading"&gt;</span>
+				<span class="guide-section-sub-label">&lt;h{n} class="small-heading"&gt;</span>
 			</div>
 			
 			<div class="large-8 large-pull-4 columns">
@@ -91,20 +141,158 @@
 		</div>
 	</section>
 
-	<section id="text-field" class="guide-section">
+	<section id="select-field" class="guide-section">
 		<div class="row">
 			<div class="large-4 large-push-8 columns">
-				<h2 class="guide-section-label">Form Select Field</h2>
+				<h2 class="guide-section-label">Select Box</h2>
 				<span class="guide-section-sub-label">&lt;select/&gt;</span>
 			</div>
 			
 			<div class="large-8 large-pull-4 columns">
-				<label>Select label</label>
 				<select>
 					<option>Option One</option>
 					<option>Option Two</option>
 					<option>Option Three</option>
 				</select>
+			</div>
+		</div>
+	</section>
+
+	<section id="radio-buttons" class="guide-section">
+		<div class="row">
+			<div class="large-4 large-push-8 columns">
+				<h2 class="guide-section-label">Radio Buttons</h2>
+				<span class="guide-section-sub-label">&lt;input type="radio"/&gt;</span>
+			</div>
+			
+			<div class="large-8 large-pull-4 columns">
+				<input type="radio" name="radios" id="radio-1" checked><label for="radio-1">Option One</label>
+				<input type="radio" name="radios" id="radio-2"><label for="radio-2">Option Two</label>
+				<input type="radio" name="radios" id="radio-3"><label for="radio-3">Option Three</label>
+			</div>
+		</div>
+	</section>
+
+	<section id="checkboxes" class="guide-section">
+		<div class="row">
+			<div class="large-4 large-push-8 columns">
+				<h2 class="guide-section-label">Checkboxes</h2>
+				<span class="guide-section-sub-label">&lt;input type="radio"/&gt;</span>
+			</div>
+			
+			<div class="large-8 large-pull-4 columns">
+				<input type="checkbox" id="checkbox-1" checked><label for="checkbox-1">Option One</label>
+				<input type="checkbox" id="checkbox-2"><label for="checkbox-2">Option Two</label>
+				<input type="checkbox" id="checkbox-3"><label for="checkbox-3">Option Three</label>
+			</div>
+		</div>
+	</section>
+
+
+
+	<div class="row">
+		<div class="large-12 columns">
+			<h2 class="guide-section-heading">Buttons</h2>
+		</div>
+	</div>
+
+	<section id="large-button" class="guide-section">
+		<div class="row">
+			<div class="large-4 large-push-8 columns">
+				<h2 class="guide-section-label">Large Button</h2>
+				<span class="guide-section-sub-label">&lt;a class="large button"&gt;</span>
+			</div>
+			
+			<div class="large-8 large-pull-4 columns">
+				<a class="large button">Click Me</a>
+			</div>
+		</div>
+	</section>
+	<section id="medium-button" class="guide-section">
+		<div class="row">
+			<div class="large-4 large-push-8 columns">
+				<h2 class="guide-section-label">Medium Button</h2>
+				<span class="guide-section-sub-label">&lt;a class="button"&gt;</span>
+			</div>
+			
+			<div class="large-8 large-pull-4 columns">
+				<a class="button">Click Me</a>
+			</div>
+		</div>
+	</section>
+	<section id="small-button" class="guide-section">
+		<div class="row">
+			<div class="large-4 large-push-8 columns">
+				<h2 class="guide-section-label">Small Button</h2>
+				<span class="guide-section-sub-label">&lt;a class="small button"&gt;</span>
+			</div>
+			
+			<div class="large-8 large-pull-4 columns">
+				<a class="small button">Click Me</a>
+			</div>
+		</div>
+	</section>
+	<section id="tiny-button" class="guide-section">
+		<div class="row">
+			<div class="large-4 large-push-8 columns">
+				<h2 class="guide-section-label">Tiny Button</h2>
+				<span class="guide-section-sub-label">&lt;a class="tiny button"&gt;</span>
+			</div>
+			
+			<div class="large-8 large-pull-4 columns">
+				<a class="tiny button">Click Me</a>
+			</div>
+		</div>
+	</section>
+
+
+
+	<div class="row">
+		<div class="large-12 columns">
+			<h2 class="guide-section-heading">User Interface Elements</h2>
+		</div>
+	</div>
+
+	<section id="breadcrumbs" class="guide-section">
+		<div class="row">
+			<div class="large-4 large-push-8 columns">
+				<h2 class="guide-section-label">Breadcrumbs</h2>
+				<span class="guide-section-sub-label">&lt;ul class="breadcrumbs"&gt;<br>&nbsp;&lt;li class="unavailable"&gt;<br>&nbsp;&lt;li class="current"&gt;</span>
+			</div>
+			
+			<div class="large-8 large-pull-4 columns">
+				<ul class="breadcrumbs">
+					<li><a href="#">Home</a></li>
+					<li><a href="#">Page</a></li>
+					<li class="unavailable"><a href="#">Sub Page</a></li>
+					<li class="current"><a href="#">Sub Sub Page</a></li>
+				</ul>
+			</div>
+		</div>
+	</section>
+
+
+	<section id="pagination" class="guide-section">
+		<div class="row">
+			<div class="large-4 large-push-8 columns">
+				<h2 class="guide-section-label">Pagination</h2>
+				<span class="guide-section-sub-label"><nav class="pagination">&lt;nav class="pagination"&gt;<br>&nbsp;&lt;ul class="page-numbers"&gt;<br>&nbsp;&nbsp;&lt;li class="current"&gt;</span>
+			</div>
+			
+			<div class="large-8 large-pull-4 columns">
+				<nav class="pagination">
+					<ul class="page-numbers">
+						<li><a class="prev page-numbers" href="#">« Previous</a></li>
+						<li><a class="page-numbers" href="#">1</a></li>
+						<li><a class="page-numbers" href="#">2</a></li>
+						<li><span class="page-numbers current">3</span></li>
+						<li><a class="page-numbers" href="#">4</a></li>
+						<li><a class="page-numbers" href="#">5</a></li>
+						<li><span class="page-numbers dots">…</span></li>
+						<li><a class="page-numbers" href="#">7</a></li>
+						<li><a class="next page-numbers" href="#">Next »</a></li>
+					</ul>
+				</nav>
 			</div>
 		</div>
 	</section>
