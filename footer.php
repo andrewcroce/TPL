fa3a865e272540207e2b11bdcb1876c945ad3525
Andrew Croce <andrew.croce@gmail.com>
2015-05-19 08:59:20 -0400

    Add default footer contents

--- footer.php
+++ footer.php
@@ -1,4 +1,16 @@
+		<footer id="main-footer">
+
+			<?php tpl('wrapper','12col-start'); ?>
+			
+				<small>©<?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?></small>
+
+			<?php tpl('wrapper','12col-end'); ?>
+
+		</footer>
+
 	</main>
+
 	<?php wp_footer(); ?>
+
 </body>
 </html>
--- tpl_contents/content-default.php
+++ tpl_contents/content-default.php
@@ -1,8 +1,16 @@
 <article class="content-default">
-			
-	<h1><?php echo $content->post_title; ?></h1>
+	
+	<header>
 
-	<time><?php echo $content->post_date->format('F j, Y'); ?></time>
+		<h1><?php echo $content->post_title; ?></h1>
+
+		<time datetime="<?php echo $content->post_date->format('Y-m-d H:i'); ?>">
+
+			<?php echo $content->post_date->format('F j, Y'); ?>
+
+		</time>
+		
+	</header>		
 
 	<?php echo $content->filterContent('post_content'); ?>
 			
