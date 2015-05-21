4c43d378c0ba751d70640b45f106e84141e3bfeb
Andrew Croce <andrew.croce@gmail.com>
2015-05-21 13:08:37 -0400

    Add variable containing ul classes to main nav template

--- tpl_navs/nav-main.php
+++ tpl_navs/nav-main.php
@@ -1,6 +1,13 @@
-<nav id="main-menu" role="navigation">
+<?php 
+/*
+ * Main Navigation template
+ * @var string $nav Class names to add to the menu <ul>
+ *
+ */ ?>
+
+<nav id="main-navigation" role="navigation">
 	<?php wp_nav_menu( array( 
 		'theme_location' => 'main_menu',
-		'items_wrap' => '<ul id="%1$s" class="inline-list %2$s">%3$s</ul>'
+		'items_wrap' => '<ul id="%1$s" class="'.$nav.' %2$s">%3$s</ul>'
 	));?>
 </nav>
