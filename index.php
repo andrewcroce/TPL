63292720f7864556b44cb307d5b02f74040b69f3
Andrew Croce <andrew.croce@gmail.com>
2015-05-28 17:26:43 -0400

    Add search results functionality to default index template
    Update file comments

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
-<?php 
-
-// This is the only template file required for a WP theme to function.
-// In most cases, this template will be overridden by a more specific template.
-// Refer to http://codex.wordpress.org/images/1/18/Template_Hierarchy.png
+<?php /**
+ * This is the default template file for Wordpress.
+ * We use it to display basic lists of posts, including search results.
+ * In many cases, this will be overridden by a more specific template.
+ * 
+ **/
 
 ?>
 <?php get_header(); ?>
@@ -10,8 +11,19 @@
 <?php if( have_posts() ) : ?>
 
 	<?php tpl('wrapper','12col-start'); ?>
+		
+		<?php // If this is a search, extract some useful pagination variables and display a search header
+		if( is_search() ) : extract( get_paged_vars( $wp_query ) ); ?>
 
-		<?php extract( get_paged_vars( $paged ) ); ?>
+			<header>
+
+				<h1><?php echo sprintf(__('Search results for "%s"'), get_query_var('s')); ?></h1>
+
+				<p><?php echo sprintf(__('Showing %s - %s of %s results'), $start_number, $end_number, $total_number); ?></p>
+				
+			</header>
+			
+		<?php endif; ?>
 
 		<ol class="item-list page-<?php echo $page_number; ?>" start="<?php echo $start_number; ?>">
 
