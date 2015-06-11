1ce793ee90b558dd1fc22eafbfaca0d18c91a696
Andrew Croce <andrew.croce@gmail.com>
2015-06-11 12:21:13 -0400

    Add jquery-transporter and jquery-throttle-debounce as js dependencies

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 // Import the foundation base script, add additional libraries/modules as needed
 // @codekit-prepend "../bower_components/foundation/js/foundation.js"
 // @codekit-prepend "../bower_components/uri.js/src/URI.js"
+// @codekit-prepend "../bower_components/jquery-throttle-debounce/jquery.ba-throttle-debounce.js"
+// @codekit-prepend "../bower_components/jquery-transporter/src/transporter.js"
 // @codekit-prepend "_taxonomy_filters.js"
 
 
