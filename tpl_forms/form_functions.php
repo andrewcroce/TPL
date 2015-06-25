b4e6ec3526b74e513ce176afbaf7412a582dae78
Andrew Croce <andrew.croce@gmail.com>
2015-06-25 16:34:34 -0400

    profile form and login form template functions

--- tpl_forms/form_functions.php
+++ tpl_forms/form_functions.php
@@ -2,14 +2,43 @@
 
 /**
  * Include the login form template
+ * @param string 		$form_id 			Text to use for the HTML form ID
+ * @param string/int 	$redirect 			Page ID or slug to redirect to on login
  */
 function tpl_form_login( $form_id, $redirect = null ) {
 
 	if( is_null( $redirect ) )
-		$redirect = ( is_ssl() ? 'https://' : 'http://' ) . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+		$redirect = 'profile';
+
+	if( is_int( $redirect ) ) {
+		$redirect_url = get_permalink( $redirect );
+	} else {
+		$redirect_url = get_permalink( get_page_by_path( $redirect ) );
+	}
 
 	tpl( 'form' , 'login', array(
 		'form_id' => $form_id,
-		'redirect' => $redirect
+		'redirect_url' => $redirect_url
 	));
+}
+
+
+
+/**
+ * Include the profile form template 
+ * @param WP_User $user
+ */
+function tpl_form_profile( $user = null ) {
+
+	if( is_null( $user ) )
+		$user = get_userdata( get_current_user_id() );
+
+	if( $user ){
+
+		tpl( 'form' , 'profile' , array(
+			'user' => $user
+		));
+
+	}
+	
 }
