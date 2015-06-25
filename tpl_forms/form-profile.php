1f2ba30d9f2a8460f307187299351b7e7665dbf9
Andrew Croce <andrew.croce@gmail.com>
2015-06-25 16:35:52 -0400

    Profile form template. Just a var dump for now

---
+++ tpl_forms/form-profile.php
@@ -0,0 +1,15 @@
+<?php 
+/**
+ * Profile form template - tpl_form_profile()
+ * 
+ * @var array $params {
+ *      
+ *      Parameters passed into the template from tpl_form_profile() 
+ *      
+ *		 @var WP_User $user WP User object
+ * }	
+ * 
+ */ 
+extract( $params ); ?>
+
+<?php dump( $user ); ?>
