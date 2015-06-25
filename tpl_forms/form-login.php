cb601a9c3431c591c15fd90e47f4992a3f5abe8f
Andrew Croce <andrew.croce@gmail.com>
2015-06-25 16:35:37 -0400

    Login form template

--- tpl_forms/form-login.php
+++ tpl_forms/form-login.php
@@ -5,8 +5,8 @@
  * @var array $params {
  *      
  *      Parameters passed into the template from tpl_form_login() 
- *      @var string $form_id
- *      @var string $redirect
+ *      @var string 	$form_id
+ *      @var string 	$redirect_url
  * }	
  * 
  */ 
@@ -14,73 +14,61 @@ extract( $params ); ?>
 
 <?php if( ! is_user_logged_in() ) : ?>
 
-	<section id="member-login-section">
 
-		<?php tpl_wrapper_secondary_open(); ?>
+	<?php if( $error = get_query_var( 'login_error', 0 ) ) {
 
-			<h3><?php echo __('Member Login','theme'); ?></h3>
+		if( $error == 'failed' )
+			tpl_block_alert( __('The credentials you entered are incorrect.'), 'alert' );
 
-			<form id="<?php echo $form_id ?>" method="post" action="<?php echo $_SERVER['REQUEST_URI']; ?>">
+		if( $error == 'username_email' )
+			tpl_block_alert( __('Please enter your email or username.'), 'alert' );
 
-				<label for="login-username-email_<?php echo $form_id; ?>"><?php echo __('Email or Username','theme'); ?></label>
-				<input id="login-username-email_<?php echo $form_id; ?>" type="text" name="params[login_username_email]">
+		if( $error == 'password' )
+			tpl_block_alert( __('Please enter your password.'), 'alert' );
+	
+	} ?>
 
-				<label for="login-password_<?php echo $form_id; ?>"><?php echo __('Password','theme'); ?></label>
-				<input id="login-password_<?php echo $form_id; ?>" type="password" name="params[login_password]">
-				
-				<input type="hidden" name="params[redirect]" value="<?php echo $redirect; ?>">
-				
-				<?php wp_nonce_field( 'handle_member_login', 'member_login_nonce' ); ?>
-				
-				<input type="hidden" name="form_action" value="handle_member_login">
-				<button type="submit"><?php echo __('Login','theme'); ?></button>
-				
-			</form>
+	
+	<?php
 
-			<?php 
+	wp_login_form(array(
 
-			// wp_login_form(array(
+		// (boolean) (optional) Display the results
+		'echo' => 1,
 
-			// 	// (boolean) (optional) Display the results
-			// 	'echo' => 1,
+		// (optional) URL to redirect to. Must be absolute (as in, http://example.com/mypage/)
+		'redirect' => $redirect_url,
 
-			// 	// (optional) URL to redirect to. Must be absolute (as in, http://example.com/mypage/)
-			// 	'redirect' => ( is_ssl() ? 'https://' : 'http://' ) . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'],
+		// (optional) form_id
+		'form_id' => $form_id,
 
-			// 	// (optional) form_id
-			// 	'form_id' => 'loginform',
+		'label_username' => __( 'Email or Username', 'theme' ),
 
-			// 	'label_username' => __( 'Username', 'theme' ),
+		'label_password' => __( 'Password', 'theme' ),
+			
+			'label_remember' => __( 'Remember Me', 'theme' ),
+		
+		'label_log_in' => __( 'Log In', 'theme' ),
 
-			// 	'label_password' => __( 'Password', 'theme' ),
-       			
-   //     			'label_remember' => __( 'Remember Me', 'theme' ),
-        		
-   //      		'label_log_in' => __( 'Log In', 'theme' ),
+		'id_username' => 'user_login',
+		
+		'id_password' => 'user_pass',
+		
+		'id_remember' => 'rememberme',
+		
+		'id_submit' => 'wp-submit',
 
-   //      		'id_username' => 'user_login',
-        		
-   //      		'id_password' => 'user_pass',
-        		
-   //      		'id_remember' => 'rememberme',
-        		
-   //      		'id_submit' => 'wp-submit',
+		// (optional) Whether to remember the values
+		'remember' => 1,
 
-   //      		// (optional) Whether to remember the values
-			// 	'remember' => 1,
+		// (optional) value_username
+		'value_username' => null,
 
-			// 	// (optional) value_username
-			// 	'value_username' => null,
+		// (optional)
+		'value_remember' => 0,
 
-			// 	// (optional)
-			// 	'value_remember' => 0,
+	)); 
 
-			// )); 
-
-			?>
-
-		<?php tpl_wrapper_secondary_close(); ?>
-
-	</section>
+	?>
 		
 <?php endif ?>
