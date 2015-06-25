f310fe6e5e90f1da1e8347a204836809456fa74a
Andrew Croce <andrew.croce@gmail.com>
2015-06-25 17:29:35 -0400

    Add login links to footer

--- footer.php
+++ footer.php
@@ -6,7 +6,23 @@
 						
 						<div class="large-12 columns">
 
-							<small>©<?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?></small>
+							<ul class="inline-list secondary-text">
+							
+								<li><a href="<?php echo home_url(); ?>">©<?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?></a></li>
+
+								<?php if( is_user_logged_in() ) : ?>
+									
+									<li><a href="<?php echo wp_logout_url(); ?>"><?php echo __('Logout','theme'); ?></a></li>
+
+									<li><a href="<?php echo home_url('profile'); ?>"><?php echo __('Profile','theme'); ?></a></li>
+								
+								<?php else : ?>
+									
+									<li><a href="<?php echo home_url('login'); ?>"><?php echo __('Login','theme'); ?></a></li>
+
+								<?php endif; ?>
+
+							</ul>
 							
 						</div>
 
