0e0fa45bbbfb82778526badc2194ec4a32fffd53
Andrew Croce <andrew.croce@gmail.com>
2015-06-25 21:16:59 -0400

    Start to build user profile form

--- tpl_forms/form-profile.php
+++ tpl_forms/form-profile.php
@@ -7,9 +7,28 @@
  *      Parameters passed into the template from tpl_form_profile() 
  *      
  *		 @var WP_User $user WP User object
+ *		 @var onject $user_meta Object containing all usermeta fields
  * }	
  * 
  */ 
 extract( $params ); ?>
 
+<?php if( is_user_logged_in() ) : ?>
+	
+	<form method="post" action="<?php the_permalink(); ?>">
+		
+		<label for="email-input"><?php echo __('Email') ?></label>
+		<input type="email" id="email-input" name="params[data][user_email]" value="<?php echo $user->user_email; ?>">
+
+		<label for="firstname-input"><?php echo __('First Name') ?></label>
+		<input type="email" id="firstname-input" name="params[meta][first_name]" value="<?php echo $user_meta->first_name; ?>">
+
+		<?php echo $user->user_email; ?>
+
+	</form>
+
+<?php endif; ?>
+
 <?php dump( $user ); ?>
+
+<?php dump( $user_meta ); ?>
