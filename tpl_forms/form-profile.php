49825f61ab8a806dcb60332bc87fa4540a15fbcc
Andrew Croce <andrew.croce@gmail.com>
2015-06-28 14:57:03 -0400

    More profile form

--- tpl_forms/form-profile.php
+++ tpl_forms/form-profile.php
@@ -15,7 +15,7 @@ extract( $params ); ?>
 
 <?php if( is_user_logged_in() ) : ?>
 	
-	<form method="post" action="<?php the_permalink(); ?>">
+	<form method="post" action="<?php the_permalink(); ?>" data-abide>
 
 		<div class="row">
 
@@ -23,12 +23,12 @@ extract( $params ); ?>
 
 			<div class="large-4 large-push-2 medium-5 medium-push-1 small-12 columns">
 				<label for="firstname-input"><?php echo __('First Name') ?></label>
-				<input type="email" id="firstname-input" name="params[meta][first_name]" value="<?php echo $user_meta->first_name; ?>">
+				<input type="text" id="firstname-input" name="params[meta][first_name]" value="<?php echo $user_meta->first_name; ?>">
 			</div>
 
 			<div class="large-4 large-pull-2 medium-5 medium-pull-1 small-12 columns">
-				<label for="firstname-input"><?php echo __('First Name') ?></label>
-				<input type="email" id="firstname-input" name="params[meta][first_name]" value="<?php echo $user_meta->first_name; ?>">
+				<label for="lastname-input"><?php echo __('Last Name') ?></label>
+				<input type="text" id="lastname-input" name="params[meta][last_name]" value="<?php echo $user_meta->last_name; ?>">
 			</div>
 			
 		</div>
@@ -36,7 +36,8 @@ extract( $params ); ?>
 		<div class="row">
 			<div class="large-8 large-centered medium-10 medium-centered small-12 columns">
 				<label for="email-input"><?php echo __('Email') ?></label>
-				<input type="email" id="email-input" name="params[data][user_email]" class="check-pass-strength-email" data-pass-id="profile-form-pass" value="<?php echo $user->user_email; ?>">
+				<input required type="email" id="email-input" name="params[data][user_email]" class="check-pass-strength-email" data-pass-id="profile-form-pass" value="<?php echo $user->user_email; ?>">
+				<small class="error"><?php echo __('An email address is required'); ?></small>
 			</div>
 		</div>
 
@@ -53,6 +54,15 @@ extract( $params ); ?>
 			<div class="large-3 large-pull-2 medium-4 medium-pull-1 small-12 columns">
 				<label for="password-strength-meter"><?php echo __('Password strength','theme'); ?></label>
 				<input type="text" id="password-strength-meter" class="check-pass-strength-meter" data-pass-id="profile-form-pass" value="" readonly>
+				<input type="hidden" class="check-pass-strength-allow submit-gate" data-pass-id="profile-form-pass" value="1" required pattern="[1]">
+				<small class="error">Please enter a stronger password</small>
+			</div>
+		</div>
+
+
+		<div class="row">
+			<div class="large-8 large-centered medium-10 medium-centered columns">
+				<input type="submit" class="submit-gate-protected" data-open="1" value="<?php echo __('Save Profile','theme'); ?>">
 			</div>
 		</div>
 
