3f1bb52034bc67fc0524ebdd4b9c56428eb415f0
Brian Gantick <brian@proudpixel.com>
2015-07-13 14:33:46 -0400

    recompile after adding dependency

--- js/admin.js
+++ js/admin.js
@@ -1,19 +1,19 @@
 // Include scripts with Codekit <https://incident57.com/codekit/help.html#javascript>
-// 
+//
 // @codekit-prepend "../bower_components/jquery.repeater/jquery.repeater.js"
 // @codekit-prepend "_display_toggle.js"
 
 
 // Include scripts with gulp-inlcude <https://www.npmjs.com/package/gulp-include>
-// 
+//
 //= include ../bower_components/jquery.repeater/jquery.repeater.js
 //= include _display_toggle.js
 
 
 (function($){
-	
+
 	$(document).ready(function(){
-		
+
 		$('.repeater').repeater({
 			isFirstItemUndeletable : false,
 			defaultValues: {
@@ -28,5 +28,5 @@
 		}
 
 	});
-	
+
 })(jQuery);
